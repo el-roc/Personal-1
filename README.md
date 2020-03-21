071346655d9b3a3b478366377b1f2285438894d1
el-roc <eric.eespinoza@gmail.com>
2020-03-21 17:26:26 -0400

    commit personal 1

---
+++ README.md
@@ -0,0 +1,15 @@
+![img](public/img/project.png)
+
+
+## Fullstack Personal 1
+
+A General Forum app w/multiple pages
+
+## Tech Used
+
+Express
+MongoDB
+
+## 
+
+
---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://resilient:demo@first-fullstack-do3ba.mongodb.net/test?retryWrites=true&w=majority', // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'dbName': 'first-fullstack'
+};
